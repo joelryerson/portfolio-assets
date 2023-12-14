8585e3cc488a22b770f3d5f0fa1bcdb6d098df8e
Joel Ryerson <joelryerson@gmail.com>
2023-12-14 12:04:24 -0800

    Update portfolio-assets.js

--- portfolio-assets.js
+++ portfolio-assets.js
@@ -352,7 +352,7 @@ document.querySelectorAll(".close-modal").forEach(function (button) {
 //show focused-pressed state layer on mouse down
 
 // Grab all the buttons
-const buttons = document.querySelectorAll('.button, .icon-button, .chip, .button-segment, .accordion-header, .menu-item, .navbar-mobile-segment, .fab, .nav-segment-icon-container');
+const buttons = document.querySelectorAll('.button, .icon-button, .chip, .button-segment, .accordion-header, .menu-item, .navbar-mobile-segment, .fab, .nav-rail-segment');
 
 // Loop through each button
 buttons.forEach((button) => {
@@ -386,7 +386,7 @@ buttons.forEach((button) => {
 //ripple effect
 
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll('.button, .icon-button, .chip, .button-segment, .accordion-header, .menu-item, .navbar-mobile-segment, .fab, .nav-segment-icon-container');
+    const buttons = document.querySelectorAll('.button, .icon-button, .chip, .button-segment, .accordion-header, .menu-item, .navbar-mobile-segment, .fab');
 
     buttons.forEach(button => {
         button.addEventListener('mousedown', function (e) {
